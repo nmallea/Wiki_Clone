beb2a080c78fbbd208c22c856536313974b341a1
Nikane Mallea <nikane@mac.com>
2020-08-16 16:28:22 +0200

    Reworking views and functions
    * urls added
    * attempting django forms
    * view functions added

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
-]
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.entry_page, name="entry"),
+    path("search", views.search, name="search"),
+    path("create_entry", views.create_entry, name="create_entry"),
+    path("edit)_entry", views.edit_entry, name="edit_entry"),
+    ]
+
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,12 +1,80 @@
 from django.shortcuts import redirect, render
-from markdown2 import Markdown
-
+from markdown2 import markdown
+from django.http import HttpResponseRedirect
 from . import forms
 from . import util
 
+
+# Create your views here.
+
+#entry page, index, search, create new entry, edit entry, random, convert markdown
+
+def entry_page(request, title):
+    content = util.get_entry(title)
+    if not content:
+        return render(request, "encyclopedia/error.html", {
+            "message": "\"" + title + "\" is not found. Feel free to add to Wiki."
+        })
+    return render(request, "encyclopedia/entry_page.html", {
+        "title": title,
+        "content": markdown(content)
+    })
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def search(request):
+    search_request = request.GET.get("q")
+    content = util.get_entry(search_request)
+    if not content:
+        result = []
+        for title in util.list_entries():
+            if search_request.casefold() in title.casefold():
+                result.append(title)
+        return render(request, "encyclopedia/search.html", {
+            "result": result
+        })
+    return render(request, "encyclopedia/entry_page.html", {
+        "title": search_request,
+        "content": markdown(content)
+    })
+
+def create_entry(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        for entry in util.list_entries():
+            if title.casefold() == entry.casefold():
+                return render(request, "encyclopedia/create_entry.html", {
+                    "message": "same name msg",
+                    "title": title,
+                    "content": content
+                })
+        util.save_entry(title, content)
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries(),
+            "message": "Your entry has been added to the Encyclopedia. Thank you!"
+        })
+    return render(request, "encyclopedia/create_entry.html")
+
+def edit_entry(request, title):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        util.save_entry(title, content)
+        return render(request, "encyclopedia/entry_page.html", {
+            "title": title,
+            "content": markdown(content),
+            "message": "\"" + title + "\" has been edited and saved."
+        })
+    content = util.get_entry(title)
+    return render(request, "encyclopedia/edit_entry.html", {
+        "title": title,
+        "content": content
+    })
+
+# def random_entry(request):
+
 
