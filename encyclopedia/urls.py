01b36e2826b7b9b7bae68185f35c969a70a2909b
Nikane Mallea <nikane@mac.com>
2020-08-17 10:00:24 +0200

    Update urls.py

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry_page, name="entry"),
+    path("wiki/<str:title>", views.entry, name="entry"),
     path("search", views.search, name="search"),
-    path("create_entry", views.create_entry, name="create_entry"),
-    path("edit)_entry", views.edit_entry, name="edit_entry"),
+    path("create_entry", views.create, name="create"),
+    path("edit)_entry", views.edit, name="edit"),
     ]
 
