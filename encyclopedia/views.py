3e5de3ed5febbef5f595732246e7ee1853f56791
Nikane Mallea <nikane@mac.com>
2020-08-17 10:17:04 +0200

    Update views.py

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,13 +9,13 @@ from . import util
 
 #entry page, index, search, create new entry, edit entry, random, convert markdown
 
-def entry_page(request, title):
+def entry(request, title):
     content = util.get_entry(title)
     if not content:
         return render(request, "encyclopedia/error.html", {
-            "message": "\"" + title + "\" is not found. Feel free to add to Wiki."
+            "message": "\"" + title + "\" is not an entry. Feel free to add to Wiki."
         })
-    return render(request, "encyclopedia/entry_page.html", {
+    return render(request, "encyclopedia/entry.html", {
         "title": title,
         "content": markdown(content)
     })
@@ -36,41 +36,41 @@ def search(request):
         return render(request, "encyclopedia/search.html", {
             "result": result
         })
-    return render(request, "encyclopedia/entry_page.html", {
+    return render(request, "encyclopedia/entry.html", {
         "title": search_request,
         "content": markdown(content)
     })
 
-def create_entry(request):
+def create(request):
     if request.method == "POST":
         title = request.POST.get("title")
         content = request.POST.get("content")
         for entry in util.list_entries():
             if title.casefold() == entry.casefold():
-                return render(request, "encyclopedia/create_entry.html", {
-                    "message": "same name msg",
+                return render(request, "encyclopedia/create.html", {
+                    "message": "",
                     "title": title,
                     "content": content
                 })
         util.save_entry(title, content)
         return render(request, "encyclopedia/index.html", {
             "entries": util.list_entries(),
-            "message": "Your entry has been added to the Encyclopedia. Thank you!"
+            "message": "Your entry has been added to the Encyclopedia. Thank you for contributing!"
         })
     return render(request, "encyclopedia/create_entry.html")
 
-def edit_entry(request, title):
+def edit(request, title):
     if request.method == "POST":
         title = request.POST.get("title")
         content = request.POST.get("content")
         util.save_entry(title, content)
-        return render(request, "encyclopedia/entry_page.html", {
+        return render(request, "encyclopedia/entry.html", {
             "title": title,
             "content": markdown(content),
             "message": "\"" + title + "\" has been edited and saved."
         })
     content = util.get_entry(title)
-    return render(request, "encyclopedia/edit_entry.html", {
+    return render(request, "encyclopedia/edit.html", {
         "title": title,
         "content": content
     })
